154bcba9f209dfd1212a7ed655418bf26e98658f
Arnav Singh <sing2415@purdue.edu>
2025-10-26 01:31:25 -0400

    proper chunking, forgot to commit last time

--- openaiwhisper.py
+++ openaiwhisper.py
@@ -1,13 +1,35 @@
+#imports open ai whisper (open source code)
 import whisper
+#imports natural language toolkit tokenizer
 from nltk.tokenize import sent_tokenize
 import nltk
 nltk.download('punkt')
 nltk.download('punkt_tab')
 model = whisper.load_model("base")
+#transcribing file
 file = "1minspeech.mp3"
 result = model.transcribe(file)
+#result["text"] gives transcript as a string with proper punctuation
+#tokenizes transcript into sentences
 sentences = sent_tokenize(result["text"])
-print(result["text"])
-print("LIST OF SENTENCES")
-print(sentences)
+#creates transcript
+transcript = result["text"]
+print(transcript)
+#code to sort transcript into list of 1800 char chunks for distilbert
 chunks = []
+max_chars = 1800
+chunk = ""
+current = 0
+for sentence in sentences:
+    if current + len(sentence) > max_chars:
+        chunks.append(chunk.strip())
+        chunk = ""
+        current = 0
+        while len(sentence) > max_chars:
+            chunks.append(sentence[:max_chars])
+            sentence = sentence[max_chars:]
+    chunk += " " + sentence
+    current += len(sentence)
+if chunk.strip():
+    chunks.append(chunk.strip())
+print(chunks)
